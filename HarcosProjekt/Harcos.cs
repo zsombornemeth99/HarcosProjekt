9fc7999ad6412ca7cc6f4b466f8f82354b346667
Zsombor Németh <zsombor.nemeth99@gmail.com>
2020-09-29 11:55:07 +0200

    Harcos osztály ToString metódus + Megkuzd metodus

--- HarcosProjekt/Harcos.cs
+++ HarcosProjekt/Harcos.cs
@@ -26,31 +26,31 @@ namespace HarcosProjekt
                 this.alapSebzes = 3;
                 this.eletero = MaxEletero;
             }
-            else if (statuszSablon==2)
+            else if (statuszSablon == 2)
             {
                 this.alapEletero = 12;
                 this.alapSebzes = 4;
                 this.eletero = MaxEletero;
             }
-            else if (statuszSablon==3)
+            else if (statuszSablon == 3)
             {
                 this.alapEletero = 8;
                 this.alapSebzes = 5;
                 this.eletero = MaxEletero;
             }
-            
+
         }
 
         public string Nev { get => nev; set => nev = value; }
         public int Szint { get => szint; set => szint = value; }
         public int Tapasztalat { get => tapasztalat; set => tapasztalat = value; }
         public int Eletero { get => eletero; set => eletero = value; }
-        public int AlapEletero { get => alapEletero;}
-        public int AlapSebzes { get => alapSebzes;}
-        
+        public int AlapEletero { get => alapEletero; }
+        public int AlapSebzes { get => alapSebzes; }
+
         public int Sebzes
         {
-            get => AlapSebzes+Szint;
+            get => AlapSebzes + Szint;
         }
 
         public int SzintLepeshez
@@ -61,6 +61,37 @@ namespace HarcosProjekt
         public int MaxEletero
         {
             get => AlapEletero + szint * 3;
+        }
+
+        public void Megkuzd(Harcos masikHarcos)
+        {
+            if (this == masikHarcos)
+                Console.WriteLine("Hiba!");
+            else if (this.eletero == 0 || masikHarcos.eletero == 0)
+                Console.WriteLine("Hiba!");
+            else
+            {
+                masikHarcos.eletero -= this.Sebzes;
+                if (masikHarcos.eletero > 0)
+                {
+                    this.Eletero -= masikHarcos.Sebzes;
+                }
+
+                if (this.eletero > 0)
+                    this.Tapasztalat += 5;
+                if (masikHarcos.eletero > 0)
+                    masikHarcos.Tapasztalat += 5;
+                if (this.eletero <= 0)
+                    masikHarcos.Tapasztalat += 10;
+                if (masikHarcos.eletero <= 0)
+                    this.Tapasztalat += 10;
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} - LVL:{1} - EXP:{2}/{3} - HP:{4}/{5} - DMG:{6}",
+                nev, szint, tapasztalat, SzintLepeshez, eletero, MaxEletero, Sebzes);
         }
     }
 }
