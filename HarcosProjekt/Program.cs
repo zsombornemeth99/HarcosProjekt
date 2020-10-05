ca197ae65d67464c29ea556200708bcfc5115a6a
Zsombor Németh <zsombor.nemeth99@gmail.com>
2020-10-05 15:29:07 +0200

    harcos 1.csv beolvasása, a harcosok hozzáadása a listához és kiiratás

--- HarcosProjekt/Program.cs
+++ HarcosProjekt/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,36 @@ namespace HarcosProjekt
     {
         static void Main(string[] args)
         {
+            var harcosLista = new List<Harcos>();
+
+            try
+            {
+                StreamReader r = new StreamReader("harcosok 1.csv", Encoding.UTF8);
+                while (!r.EndOfStream)
+                {
+                    string[] st = r.ReadLine().Split(';');
+
+                    harcosLista.Add(new Harcos(st[0],int.Parse(st[1])));
+                }
+                r.Close();
+
+                foreach (var item in harcosLista)
+                {
+                    Console.WriteLine(item);
+                    Console.WriteLine();
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("A fájl nem található!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Hiba: " + ex);
+            }
+
+
+            Console.ReadKey();
         }
     }
 }
